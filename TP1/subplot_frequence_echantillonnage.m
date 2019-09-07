67afff1f9bc3785144dc740c143919af99edc525
HuppeJ <jeremie.huppe@gmail.com>
2019-09-07 15:28:01 -0400

    Reviewed Partie 1

--- TP1/subplot_frequence_echantillonnage.m
+++ TP1/subplot_frequence_echantillonnage.m
@@ -1,13 +1,12 @@
 function [] = subplot_frequence_echantillonnage(position, frequence, Y)
-%SUBPLOT_FREQUENCE_ECHANTILLONNAGE Summary of this function goes here
-%   Detailed explanation goes here
+% Cette fonction ajoute un subplot à la figure regroupant les six 
+% représentations de Y(t) avec différentes fréquences d'échantillonage  
     subplot(6,1,position);
     t = linspace(0, 1, frequence);
     plot(t, Y(t));
     ylim([-20, 20]);
-    title(strcat({'T en fonction de t pour une fréquence d''échantillonnage de '}, int2str(frequence), {' Hz'}));
+    title(strcat({'Y en fonction de t pour une fréquence d''échantillonnage de '}, int2str(frequence), {' Hz'}));
     xlabel('t');
-    ylabel('Y');
-    legend('Y');
+    ylabel('Y(t)');
 end
 
