00edf3c8bb3d3c3d23efa729fd07b9302d231ead
Antoine Daigneault-Demers <antoine.d.demers@hotmail.com>
2019-10-16 13:06:01 -0400

    Fin Q1

---
+++ TP2/Rehaussement_Contour.m
@@ -0,0 +1,15 @@
+function [imageRehaussee] = Rehaussement_Contour(image, k)
+    masqueGaussien = 1/16 * [1 2 1; 2 4 2; 1 2 1];
+    Ig = Convolution(image, masqueGaussien);
+    
+    masqueLaplacien = [-1 -1 -1; -1 8 -1; -1 -1 -1];
+    laplacien = Convolution(Ig, masqueLaplacien);
+    imshow(laplacien);
+    
+    imageRehaussee = Ig + k * laplacien;
+    
+    % Mettre les valeurs négatives à 0
+    imageRehaussee(imageRehaussee < 0) = 0;
+    
+    imageRehaussee = uint8(imageRehaussee);
+end
