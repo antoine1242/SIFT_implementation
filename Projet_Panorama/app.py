082be3946b67858d49f7ee707aaac30b70dd0cc9
HuppeRault <jeremie321go@gmail.com>
2019-11-19 15:53:15 -0500

    first draft orientation

--- Projet_Panorama/app.py
+++ Projet_Panorama/app.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from difference_de_gaussiennes import difference_de_gaussiennes
 from detectionPointsCles import detectionPointsCles, isExtremum
 import cv2
+from skimage.io import imread
+from skimage.color import rgb2gray
 
 def run():
     print("Project Pano is running!")
     
-    img = cv2.imread("./images/lena_claire.jpg", cv2.IMREAD_COLOR)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = imread("./images/droite.jpg")
+    img = rgb2gray(img)
+    #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     """
     print(type(img))
     cv2.imshow('image',img)
@@ -17,8 +21,21 @@ def run():
     """
     dogs, sigmas, gaussian_filtered_images, gaussian_filtered_images_sigmas = difference_de_gaussiennes(img, 3, 2)
 
-    print("DETECTION DE POINTS CLES")
-    #detectionPointsCles(dogs[0], sigmas[0], 0, 0, 0)
+    detectionPointsCles(dogs[0], sigmas[0], 0.03, 10, 0, gaussian_filtered_images[0], gaussian_filtered_images_sigmas[0])
+
+    """
+    fig = plt.figure(figsize=(2,3))
+    for i in range(1, 6+1):
+        fig.add_subplot(2, 3, i)
+        plt.imshow(gaussian_filtered_images[1][i-1], cmap='gray', vmin=0, vmax=1)
+    plt.show()
+
+    fig = plt.figure(figsize=(2,3))
+    for i in range(1, 6):
+        fig.add_subplot(2, 3, i)
+        plt.imshow(dogs[1][i-1], cmap='gray', vmin=0, vmax=1)
+    plt.show()
+    """
 
     """
     previous = np.array([[9, 9, 9], [9, 9, 9], [9, 9, 9]])
--- Projet_Panorama/detectionPointsCles.py
+++ Projet_Panorama/detectionPointsCles.py
@@ -1,31 +1,112 @@
-def detectionPointsCles(dog, sigma, seuil_contraste=0.03, r_courbure_principale, resolution_octave):
-    candidate_keypoint = []
-    print(len(sigma))
+import numpy as np
+
+def detectionPointsCles(dog, sigma, seuil_contraste, r_courbure_principale, resolution_octave, gaussian_filtered_images, gaussian_filtered_images_sigmas):
+    print("detectionPointsCles")
+    cnt_candidates_curr = 0
+    cnt_candidates_octave = []
+    cnt_removed_contrast = 0
+    cnt_removed_edge = 0
+    keypoints = []
+
     for i in range(1, len(dog)-1):
         previous = dog[i-1]
         current = dog[i]
         nextDog = dog[i+1]
-
+        candidate_keypoints = []
         for x in range(1, len(previous)-1):
             for y in range(1, len(previous[0])-1):
-                if isExtremum(previous, current, nextDog, x,y):
-                    candidate_keypoint.append((x,y,sigma[i]))
+                if isExtremum(previous, current, nextDog, x, y):
+                    candidate_keypoints.append((x,y,sigma[i]))
+                    cnt_candidates_curr += 1
+        
+        # Élimination point clés faible constraste
+        for candidate_keypoint in candidate_keypoints:
+            stack_of_dog = np.asarray([previous, current, nextDog])
 
-    extremaDetectes = len(candidate_keypoint) 
-    
+            D_of_x, detH, ratio = dogDerivative(stack_of_dog, candidate_keypoint) 
 
+            if D_of_x < seuil_contraste:
+                cnt_removed_contrast += 1
+            elif detH < 0 or ratio > (r_courbure_principale + 1)**2 / r_courbure_principale:
+                cnt_removed_edge += 1
+            else:
+                keypoints.append(candidate_keypoint)
 
-    #for aTuple in candidate_keypoint:
-    #    print(aTuple)
+        cnt_candidates_octave.append(cnt_candidates_curr)
+        cnt_candidates_curr = 0
 
-def dogDerivative(previous, current, nextDog, x, y, sigma):
-    xDerivative = (current[x+1][y] - current[x-1][y]) / 2
-    yDerivative = (current[x][y+1] - current[x][y-1]) / 2
-    # C tu ça
-    sigmaDerivative = (nextDog[x][y] - previous[x][y]) / 2
+    cnt_candidates = sum(cnt_candidates_octave)
 
-    derivative = [xDerivative, yDerivative, sigmaDerivative]
-    
+    check_val = set()
+    keypoints_filtered = []
+    for x, y, s in keypoints:
+        if (x, y) not in check_val:
+            keypoints_filtered.append((x, y, s))
+
+    print(cnt_removed_contrast)
+    print(cnt_removed_edge)
+    print(cnt_candidates)
+    keypoints_m_and_theta = []
+    for keypoint in keypoints_filtered:
+        # TODO est-ce que le sigma est suppose etre "proche" ou exact
+        x = keypoint[0]
+        y = keypoint[1]
+        sigma = keypoint[2]
+        
+        w = int(2*np.ceil(sigma) + 1)
+
+        # s = np.clip(s, 0, octave.shape[2] - 1) ???
+        # TODO: 
+        #kernel = gaussian_filter(image_initiale_norm, sigma=sigma) 
+
+        # TODO: Comment trouver le bon L
+        # TODO: Comment trouver le nombre de pixels a observer autour
+        idx = gaussian_filtered_images_sigmas.index(sigma)
+        L = gaussian_filtered_images[idx]
+        m = ((L[x+1][y] - L[x-1][y])**2 + (L[x][y+1] - L[x][y-1])**2)**(1/2)
+        theta = np.arctan((L[x][y+1] - L[x][y-1])/(L[x+1][y] - L[x-1][y]))
+        keypoints_m_and_theta.append((x, y, sigma, m, theta))
+
+    for keypoint in keypoints_m_and_theta:
+        pass
+        # get neighbours
+        # 
+
+def dogDerivative(stack_of_dog, candidate_keypoint):
+    x = candidate_keypoint[0]
+    y = candidate_keypoint[1]
+
+    s = 1
+    dx = (stack_of_dog[s][x+1][y] - stack_of_dog[s][x-1][y]) / 2.
+    dy = (stack_of_dog[s][x][y+1] - stack_of_dog[s][x][y-1]) / 2.
+    ds = (stack_of_dog[s+1][x][y] - stack_of_dog[s-1][x][y]) / 2.
+
+    dxx = (stack_of_dog[s][x+1][y] - 2*stack_of_dog[s][x][y] + stack_of_dog[s][x-1][y]) 
+    dyy = (stack_of_dog[s][x][y+1] - 2*stack_of_dog[s][x][y] + stack_of_dog[s][x][y-1]) 
+    dss = (stack_of_dog[s+1][x][y] - 2*stack_of_dog[s][x][y] + stack_of_dog[s-1][x][y]) 
+
+    dxy = ((stack_of_dog[s][x+1][y+1] - stack_of_dog[s][x-1][y+1]) - (stack_of_dog[s][x+1][y-1] - stack_of_dog[s][x-1][y-1])) /4.
+    dxs = ((stack_of_dog[s+1][x+1][y] - stack_of_dog[s+1][x-1][y]) - (stack_of_dog[s-1][x+1][y] - stack_of_dog[s-1][x-1][y])) /4.
+    dys = ((stack_of_dog[s+1][x][y+1] - stack_of_dog[s+1][x][y-1]) - (stack_of_dog[s-1][x][y+1] - stack_of_dog[s-1][x][y-1])) /4.
+
+    d_dx = np.asarray([dx, dy, ds])
+    dd_dxx = np.asarray([
+        [dxx, dxy, dxs],
+        [dxy, dyy, dys],
+        [dxs, dys, dss]
+    ])
+
+    X = np.asarray(candidate_keypoint) # + (-np.linalg.inv(dd_dxx)).dot(d_dx)
+
+    D_of_x = stack_of_dog[s][x][y] + 0.5 * ((d_dx).dot(X)) 
+
+    # Ratio pour la courbature
+    traceH = dxx + dyy
+    detH = dxx*dyy - dxy**2
+
+    ratio = traceH**2 / detH 
+
+    return abs(D_of_x), detH, ratio
 
                 
 def isExtremum(previous, current, nextDog, x, y):
@@ -57,3 +138,4 @@ def isExtremum(previous, current, nextDog, x, y):
                 return False 
 
     return True
+
