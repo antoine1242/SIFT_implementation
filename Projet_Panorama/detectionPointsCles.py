679defa1276556f429e7faf4a8c1d245dc248e01
HuppeJ <jeremie.huppe@gmail.com>
2019-11-20 10:39:29 -0500

    added second draft orientation

--- Projet_Panorama/detectionPointsCles.py
+++ Projet_Panorama/detectionPointsCles.py
@@ -7,6 +7,9 @@ def detectionPointsCles(dog, sigma, seuil_contraste, r_courbure_principale, reso
     cnt_removed_contrast = 0
     cnt_removed_edge = 0
     keypoints = []
+    nb_bins = 10
+    bin_size = 360 // nb_bins
+
 
     for i in range(1, len(dog)-1):
         previous = dog[i-1]
@@ -49,11 +52,11 @@ def detectionPointsCles(dog, sigma, seuil_contraste, r_courbure_principale, reso
     keypoints_m_and_theta = []
     for keypoint in keypoints_filtered:
         # TODO est-ce que le sigma est suppose etre "proche" ou exact
-        x = keypoint[0]
-        y = keypoint[1]
+        x_kp = keypoint[0]
+        x_kp = keypoint[1]
         sigma = keypoint[2]
         
-        w = int(2*np.ceil(sigma) + 1)
+        range_zone = int(2*np.ceil(sigma) + 1)
 
         # s = np.clip(s, 0, octave.shape[2] - 1) ???
         # TODO: 
@@ -61,11 +64,84 @@ def detectionPointsCles(dog, sigma, seuil_contraste, r_courbure_principale, reso
 
         # TODO: Comment trouver le bon L
         # TODO: Comment trouver le nombre de pixels a observer autour
+
         idx = gaussian_filtered_images_sigmas.index(sigma)
         L = gaussian_filtered_images[idx]
-        m = ((L[x+1][y] - L[x-1][y])**2 + (L[x][y+1] - L[x][y-1])**2)**(1/2)
-        theta = np.arctan((L[x][y+1] - L[x][y-1])/(L[x+1][y] - L[x-1][y]))
-        keypoints_m_and_theta.append((x, y, sigma, m, theta))
+        
+        hist = np.zeros(nb_bins, dtype=np.float32)
+
+        for dx_zone in range(-range_zone, range_zone + 1):
+            for dy_zone in range(-range_zone, range_zone + 1):
+                x = x_kp + dx_zone
+                y = x_kp + dy_zone
+
+                if x < 0 or x > L.shape[0] - 1 or y < 0 or y > L.shape[1] - 1: 
+                    continue
+
+                dx = L[min(L.shape[0]-1, x+1)][y] - L[max(x-1, 0)][y]
+                dy = L[x][min(L.shape[1]-1, y+1)] - L[x][max(y-1, 0)]
+                m = np.sqrt(dx**2 + dy**2)
+                theta = (np.arctan(dy/dx)+np.pi) * 180/np.pi
+                
+                # TODO: ajuster poids de m 
+                #weight = kernel[oy+w, ox+w] * m
+                weight = m 
+
+                bin_number = int(np.floor(theta) // bin_size)
+                hist[bin_number] += weight
+
+        # TODO reste du code de l'article 
+
+        # max_bin = np.argmax(hist)
+        # new_kps.append([kp[0], kp[1], kp[2], fit_parabola(hist, max_bin, bin_width)])
+
+        # max_val = np.max(hist)
+        # for binno, val in enumerate(hist):
+        #     if binno == max_bin: continue
+
+        #     if .8 * max_val <= val:
+        #         new_kps.append([kp[0], kp[1], kp[2], fit_parabola(hist, binno, bin_width)])
+
+        # def fit_parabola(hist, binno, bin_width):
+        #     centerval = binno*bin_width + bin_width/2.
+
+        #     if binno == len(hist)-1: rightval = 360 + bin_width/2.
+        #     else: rightval = (binno+1)*bin_width + bin_width/2.
+
+        #     if binno == 0: leftval = -bin_width/2.
+        #     else: leftval = (binno-1)*bin_width + bin_width/2.
+            
+        #     A = np.array([
+        #         [centerval**2, centerval, 1],
+        #         [rightval**2, rightval, 1],
+        #         [leftval**2, leftval, 1]])
+        #     b = np.array([
+        #         hist[binno],
+        #         hist[(binno+1)%len(hist)], 
+        #         hist[(binno-1)%len(hist)]])
+
+        #     x = LA.lstsq(A, b, rcond=None)[0]
+        #     if x[0] == 0: x[0] = 1e-6
+        #     return -x[1]/(2*x[0])
+
+
+
+        # TODO est-ce qu'on compute toute la matrice des m et tetha avant?
+        # mag_theta_matrix = np.zeros(L.shape)
+        # for x in range(mag_theta_matrix.shape[0]):
+        #     for x in range(mag_theta_matrix.shape[1]):
+        #         dx = L[x+1][y] - L[x-1][y]
+        #         dy = L[x][y+1] - L[x][y-1]
+        #         m = np.sqrt(dx**2 + dy**2)
+        #         theta = (np.arctan(dy/dx)+np.pi) * 180/np.pi
+        #         mag_theta_matrix[x][y] = (m, theta)
+
+
+
+
+
+
+        #keypoints_m_and_theta.append((x, y, sigma, m, theta))
 
     for keypoint in keypoints_m_and_theta:
         pass
