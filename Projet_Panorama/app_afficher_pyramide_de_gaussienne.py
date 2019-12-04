423be4e7ddfaf954262832d20f365612e9c3d233
JeHuppe <jeremie.huppe@gmail.com>
2019-12-04 16:37:17 -0500

    updated difference_de_gaussiennes img

--- Projet_Panorama/app_afficher_pyramide_de_gaussienne.py
+++ Projet_Panorama/app_afficher_pyramide_de_gaussienne.py
@@ -8,23 +8,23 @@ def run():
     img = cv2.imread("./images/droite.jpg", cv2.IMREAD_COLOR).astype("float32")
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    dogs, sigmas, gaussian_filtered_images, gaussian_filtered_images_sigmas = difference_de_gaussiennes(img, s=3, nb_octave=2)
+    dogs, sigmas, gaussian_filtered_images, gaussian_filtered_images_sigmas = difference_de_gaussiennes(img, s=3, nb_octave=3)
 
-    octave = 0
+    octave = 2
 
     fig = plt.figure(figsize=(12,8))
     for i in range(1, 6+1):
         axi = fig.add_subplot(2, 3, i)
         axi.title.set_text(str(gaussian_filtered_images_sigmas[octave][i-1]))
         plt.imshow(gaussian_filtered_images[octave][i-1], cmap=plt.cm.gray)
-    plt.savefig("./images_rapport/pyramide_de_gaussiennes_octave_" + str(octave) + ".png")
+    plt.savefig("./images_rapport/pyramide_de_gaussiennes_octave_" + str(octave + 1) + ".png")
     plt.show()
 
     fig = plt.figure(figsize=(12,8))
     for i in range(1, 6):
         fig.add_subplot(2, 3, i)
         plt.imshow(dogs[octave][i-1], cmap=plt.cm.gray)
-    plt.savefig("./images_rapport/difference_de_gaussiennes_octave_" + str(octave) + ".png")
+    plt.savefig("./images_rapport/difference_de_gaussiennes_octave_" + str(octave + 1) + ".png")
     plt.show()
 
 
